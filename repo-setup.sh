adb71c7f6f7ff0b4ddf06ae5ba4206d44465f797
Tyonnchie-Berry-1996 <g5fds626m7@privaterelay.appleid.com>
2025-11-27 17:32:24 -0500

    Update repo-setup.sh

--- repo-setup.sh
+++ repo-setup.sh
@@ -1,4 +1,18 @@
-mv *.sh /home/src
+git clone https://github.com/Tyonnchie-Berry-1996/RepoAutomation.git
+git clone https://github.com/Tyonnchie-Berry-1996/Scripts-For-Dev-Env.git
+git clone https://github.com/Tyonnchie-Berry-1996/AI-Bash-Assistant-Prototype.git
+
+cd RepoAutomation/
+chmod +x setup.sh
+./setup.sh
+cd ../
+
+cd Scripts-For-Dev-Env/
+chmod +x *.py
+
+cd AI-Bash-Assistant-Prototype/
+fedpkg clone -a kernel
 cd /home/src
+
 
 
