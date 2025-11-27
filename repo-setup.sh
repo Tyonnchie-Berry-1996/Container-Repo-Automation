603f9f9cb52798e270e9be39e5f997821366ff32
Tyonnchie-Berry-1996 <g5fds626m7@privaterelay.appleid.com>
2025-11-27 17:28:24 -0500

    Update repo-setup.sh

--- repo-setup.sh
+++ repo-setup.sh
@@ -1,6 +1,3 @@
 mv *.sh /home/src
 
-git clone https://github.com/Tyonnchie-Berry-1996/RepoAutomation.git
-git clone https://github.com/Tyonnchie-Berry-1996/Scripts-For-Dev-Env.git
-git clone https://github.com/Tyonnchie-Berry-1996/AI-Bash-Assistant-Prototype.git
 
